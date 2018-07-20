fc0fe0f9cfbeebff5543dd73627db58088ff629e
春山優太 <haruyamayuuta@haruyamayuutanomacbook-puro.local>
2018-07-20 18:38:17 +0900

    やり直し３

--- SlideshowApp2/ResultViewController.swift
+++ SlideshowApp2/ResultViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
     var Image = UIImage()
@@ -18,26 +18,26 @@ class ResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let _: UIStoryboard = self.storyboard!
         // Do any additional setup after loading the view.
         imageView.image = UIImage (named: imageName)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
